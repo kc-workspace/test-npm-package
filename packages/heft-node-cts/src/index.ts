642ff8230d6a7dd7907ae3f49c7d644c996595a6
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-01-03 22:48:59 +0700

    feat: add heft node common typescript package

---
+++ packages/heft-node-cts/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @packageDocumentation
+ *
+ * A test package for validate that heft can use outside rushstack repository.
+ */
+
+console.log("hello commonjs");
+
+/**
+ * A test constant set to true for testing purpose
+ * @beta
+ */
+const test = true;
+export { test };
